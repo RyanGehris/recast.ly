65b251996150e2259c0c32b800cae285cbd87e32
Ryan Gehris <rogehris@gmail.com>
2022-11-07 17:37:29 -0800

    progress on recast.ly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,11 +10,19 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
+        <VideoList videos={exampleVideoData}/>
         <div><h5><em>videoList</em> view goes here</h5></div>
       </div>
     </div>
   </div>
 );
+
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((element) => {
+      <VideoListEntry video={element}/>
+    })}
   </div>
 );
 
@@ -14,6 +12,7 @@ VideoList.propTypes = {
   videos: PropTypes.array.isRequired,
 };
 
+import VideoListEntry from './VideoListEntry.js';
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
